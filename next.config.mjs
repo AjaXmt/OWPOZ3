4dc9ce89c6ca451a2e8443d17e854427d3b8d4a8
AjaXmt <mateusztochowicz@gmail.com>
2025-03-06 08:51:33 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  basePath: '/page',
+  basePath: '/OWPOZ3',
 }
 
 mergeConfig(nextConfig, userConfig)
